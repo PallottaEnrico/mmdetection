bceaf9264d4b1d3a5d33958f5fc9bc75594ab013
PallottaEnrico <pallottenrico@gmail.com>
2023-08-28 14:22:31 +0400

    restored train pipeline

--- configs/rtmdet/rtmdet_l_8xb32-300e_coco.py
+++ configs/rtmdet/rtmdet_l_8xb32-300e_coco.py
@@ -67,13 +67,19 @@ train_pipeline = [
     dict(type='CachedMosaic', img_scale=(640, 640), pad_val=114.0),
     dict(
         type='RandomResize',
-        scale=(640, 640),
+        scale=(1280, 1280),
         ratio_range=(0.1, 2.0),
         keep_ratio=True),
-    dict(type='RandomCrop', crop_size=(160, 160)),
+    dict(type='RandomCrop', crop_size=(640, 640)),
     dict(type='YOLOXHSVRandomAug'),
     dict(type='RandomFlip', prob=0.5),
     dict(type='Pad', size=(640, 640), pad_val=dict(img=(114, 114, 114))),
+    dict(
+        type='CachedMixUp',
+        img_scale=(640, 640),
+        ratio_range=(1.0, 1.0),
+        max_cached_images=20,
+        pad_val=(114, 114, 114)),
     dict(type='PackDetInputs')
 ]
 
