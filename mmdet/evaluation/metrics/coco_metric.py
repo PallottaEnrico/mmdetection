b25f2228b6344889fef7ad3af9d8ff8c3cc49e70
PallottaEnrico <pallottenrico@gmail.com>
2023-08-28 15:20:37 +0400

    restored train pipeline

--- mmdet/evaluation/metrics/coco_metric.py
+++ mmdet/evaluation/metrics/coco_metric.py
@@ -236,7 +236,12 @@ class CocoMetric(BaseMetric):
                 data['image_id'] = image_id
                 data['bbox'] = self.xyxy2xywh(bboxes[i])
                 data['score'] = float(scores[i])
-                data['category_id'] = self.cat_ids[label]
+                try:
+                    data['category_id'] = self.cat_ids[label]
+                except:
+                    print("FAILURE IN COCO METRICS")
+                    print(label)
+                    continue
                 bbox_json_results.append(data)
 
             if segm_json_results is None:
