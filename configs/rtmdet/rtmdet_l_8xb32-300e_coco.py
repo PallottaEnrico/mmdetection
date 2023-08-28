2b50a33454097c289f9a635d7ef49e762e522748
PallottaEnrico <pallottenrico@gmail.com>
2023-08-28 13:58:42 +0400

    120 ep

--- configs/rtmdet/rtmdet_l_8xb32-300e_coco.py
+++ configs/rtmdet/rtmdet_l_8xb32-300e_coco.py
@@ -67,27 +67,6 @@ train_pipeline = [
     dict(type='CachedMosaic', img_scale=(640, 640), pad_val=114.0),
     dict(
         type='RandomResize',
-        scale=(1280, 1280),
-        ratio_range=(0.1, 2.0),
-        keep_ratio=True),
-    dict(type='RandomCrop', crop_size=(640, 640)),
-    dict(type='YOLOXHSVRandomAug'),
-    dict(type='RandomFlip', prob=0.5),
-    dict(type='Pad', size=(640, 640), pad_val=dict(img=(114, 114, 114))),
-    dict(
-        type='CachedMixUp',
-        img_scale=(640, 640),
-        ratio_range=(1.0, 1.0),
-        max_cached_images=20,
-        pad_val=(114, 114, 114)),
-    dict(type='PackDetInputs')
-]
-
-train_pipeline_stage2 = [
-    dict(type='LoadImageFromFile', backend_args={{_base_.backend_args}}),
-    dict(type='LoadAnnotations', with_bbox=True),
-    dict(
-        type='RandomResize',
         scale=(640, 640),
         ratio_range=(0.1, 2.0),
         keep_ratio=True),
@@ -119,15 +98,13 @@ val_dataloader = dict(
     batch_size=32, num_workers=8, dataset=dict(pipeline=test_pipeline))
 test_dataloader = val_dataloader
 
-max_epochs = 5
-stage2_num_epochs = 3
+max_epochs = 120
 base_lr = 0.004
 interval = 5
 
 train_cfg = dict(
     max_epochs=max_epochs,
-    val_interval=interval,
-    dynamic_intervals=[(max_epochs - stage2_num_epochs, 1)])
+    val_interval=interval)
 
 val_evaluator = dict(proposal_nums=(100, 1, 10))
 test_evaluator = val_evaluator
@@ -171,9 +148,5 @@ custom_hooks = [
         ema_type='ExpMomentumEMA',
         momentum=0.0002,
         update_buffers=True,
-        priority=49),
-    dict(
-        type='PipelineSwitchHook',
-        switch_epoch=max_epochs - stage2_num_epochs,
-        switch_pipeline=train_pipeline_stage2)
+        priority=49)
 ]
